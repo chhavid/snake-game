9e9c64f8b5a101b828e41141106e835f7910cc09
Chhavi Dandriyal <99489321+chhavid@users.noreply.github.com>
2022-02-14 17:10:22 +0530

    Modified the statements for rules.

--- play_snake.sh
+++ play_snake.sh
@@ -1,7 +1,9 @@
 source "snake.sh"
 
 clear
-echo -e "In each level, if you reach the target in 5 or less attempts, you will get 10 points otherwise 5 points."
-echo -e "\n RULES: If you score 80 points in 10 levels, you will "WIN"\n"
-echo -e "\nPress 'l' for moving left, 'r' for moving right, 'u' for moving up and 'd' for moving down "
+echo "\nWelcome to command-line snake game\n"
+echo "RULES:\nIn each level, if you reach the target in 5 or less attempts, you will get 10 points otherwise 5 points."
+echo "If you score 80 points in 10 levels, you will "WIN"!\nIf you hit any boundry, you lose!"
+echo "\n-> Press 'l' for moving left, 'r' for right, 'u' for up and 'd' for down.\n"
+
 run_snake
